cee1b267d5f612960fed63b766552a786de3c5db
martafralves <74558958+martafralves@users.noreply.github.com>
2022-07-24 20:12:25 +0100

    edited config.yml and deploy.sh to include container_name

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ then
 fi
 
 # Create a container called node_app that is available on port 8443 from our docker image
-docker create -p 8443:8443 --name node_app $IMAGE_NAME
+docker create -p 8443:8443 --name node_app $IMAGE_NAME = $CONTAINER_NAME
 # Write the private key to a file
 echo $PRIVATE_KEY > privatekey.pem
 # Write the server key to a file
